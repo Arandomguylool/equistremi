b7ced485331b20814ed7deeb7e842363e7514a3e
Arandomguylool <102329658+Arandomguylool@users.noreply.github.com>
2022-12-13 12:04:40 -0300

    Update Options.hx

--- source/Options.hx
+++ source/Options.hx
@@ -121,6 +121,29 @@ class CpuStrums extends Option
 
 }
 
+class gradientHitboxes extends Option
+{
+	public function new(desc:String)
+	{
+		super();
+		description = desc;
+	}
+
+	public override function press():Bool
+	{
+		FlxG.save.data.gradientHitboxes = !FlxG.save.data.gradientHitboxes;
+		
+		display = updateDisplay();
+		return true;
+	}
+
+	private override function updateDisplay():String
+	{
+		return  FlxG.save.data.gradientHitboxes ? "Hitbox Pose DOWN" : "Hitbox Pose UP";
+	}
+
+}
+
 class DownscrollOption extends Option
 {
 	public function new(desc:String)
